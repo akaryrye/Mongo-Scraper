7dee36be470e39af0fb3cc5b1e796024e61e688d
akaryrye <ralldrin@gmail.com>
2019-10-20 17:43:50 -0700

    heroku mLab connection

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+var MONGODB = process.env.MONGODB || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB, {useNewUrlParser: true});
 
 
 app.get("/scrape", function(req, res) {
